e01203bd6f3898e86fe4ff1d8127bc5abf87ac6d
dameninngenn <dameninngenn.owata@gmail.com>
2012-03-21 03:12:05 +0900

    initial commit

---
+++ lib/Color/Convert/ANSIColor.pm
@@ -0,0 +1,32 @@
+package Color::Convert::ANSIColor;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Color::Convert::ANSIColor -
+
+=head1 SYNOPSIS
+
+  use Color::Convert::ANSIColor;
+
+=head1 DESCRIPTION
+
+Color::Convert::ANSIColor is
+
+=head1 AUTHOR
+
+dameninngenn E<lt>dameninngenn.owata {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
